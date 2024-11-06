8742e289d8a39529b1f539678fe8b5cf61ffc2d8
Wahyudi-Satriawan-1B-TI <wahyudisatriawanh@gmail.com>
2024-11-06 10:41:02 +0700

    Penyelesaian Tugas 1

---
+++ Tugas1.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Tugas1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int [][] survei = new int[10][6];
+
+        for (int i = 0; i < survei.length; i++) {
+            System.out.println("============================================================================");
+            System.out.println("Silahkan isi pertanyaan-pertanyaan berikut dengan cara melakukan input (1-5)");
+            System.out.println("1 = Sangat Tidak Puas");
+            System.out.println("2 = Tidak Puas");
+            System.out.println("3 = Biasa");
+            System.out.println("4 = Puas");
+            System.out.println("5 = Sangat Puas");
+            System.out.println("============================================================================");
+            System.out.println("Responden ke-" + (i + 1));
+            double totalPerResponden = 0;
+            
+            for (int j = 0; j < survei[i].length; j++) {
+                int input;
+                while (true) {
+                    System.out.print("Pertanyaan ke-" + (j + 1) + ": ");
+                    input = sc.nextInt();
+                    
+                    if (input >= 1 && input <= 5) {
+                        survei[i][j] = input;
+                        totalPerResponden += input;
+                        break;
+                    } else {
+                        System.out.println("Input tidak valid, masukkan angka antara 1 hingga 5.");
+                    }
+                }
+            }
+            System.out.printf("Nilai rata-rata responden ke-" + (i+1) + ": " + (totalPerResponden/6));
+            System.out.println();
+        }
+        System.out.println("============================================================================");
+        System.out.println("Nilai rata-rata setiap Pertanyaan:");
+        
+        for (int j = 0; j < 6; j++) {
+            double totalPerPertanyaan = 0;
+            
+            for (int i = 0; i < 10; i++) {
+                totalPerPertanyaan += survei[i][j];
+            }
+            System.out.printf("Pertanyaan ke-" + (j + 1) + ": " + (totalPerPertanyaan/10));
+            System.out.println();
+            System.out.println("======================================");
+        }
+        
+        sc.close();
+    }
+}
